c7aae83606a29ca542d10c503dfb2e6a9f7a8b4b
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2020-10-07 17:57:36 +0800

    增加非法CQ码调用检查 适配部分C#9特性

--- Sora_Test/Program.cs
+++ Sora_Test/Program.cs
@@ -7,38 +7,31 @@ using Sora.Tool;
 
 namespace Sora_Test
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             try
             {
-                new Program().MainAsync().GetAwaiter().GetResult();
+                ConsoleLog.SetLogLevel(LogLevel.Debug);
+                OnebotWSServer server = new OnebotWSServer(new ServerConfig());
+                server.OnOpenConnectionAsync += (id, eventArgs) =>
+                                                {
+                                                    ConsoleLog.Debug("Sora_Test",$"selfId = {id} type = {eventArgs.Role}");
+                                                    return ValueTask.CompletedTask;
+                                                };
+        
+                server.OnCloseConnectionAsync += (id, eventArgs) =>
+                                                 {
+                                                     ConsoleLog.Debug("Sora_Test",$"selfId = {id} type = {eventArgs.Role}");
+                                                     return ValueTask.CompletedTask;
+                                                 };
+                await Task.Delay(-1);
             }
             catch (Exception e) //侦测所有未处理的错误
             {
                 ConsoleLog.UnhandledExceptionLog(e);
             }
         }
-
-        private async Task MainAsync()
-        {
-            ConsoleLog.SetLogLevel(LogLevel.Debug);
-            OnebotWSServer server = new OnebotWSServer(new ServerConfig());
-            server.OnOpenConnectionAsync += (id, args) =>
-                                            {
-                                                ConsoleLog.Debug("Sora_Test",$"selfId = {id} type = {args.Role}");
-                                                return ValueTask.CompletedTask;
-                                            };
-        
-            server.OnCloseConnectionAsync += (id, args) =>
-                                             {
-                                                 ConsoleLog.Debug("Sora_Test",$"selfId = {id} type = {args.Role}");
-                                                 return ValueTask.CompletedTask;
-                                             };
-            await Task.Delay(-1);
-        }
-
-
     }
 }
