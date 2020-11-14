7dce853774f797aab0dcdb6ed197dd33e7e371fb
脆饼干 <cbgancna@gmail.com>
2020-11-15 01:42:03 +0800

    在连接事件参数中加入selfid字段

--- Sora/EventArgs/WSSeverEvent/ConnectionEventArgs.cs
+++ Sora/EventArgs/WSSeverEvent/ConnectionEventArgs.cs
@@ -1,25 +1,27 @@
-using Fleck;
-
 namespace Sora.EventArgs.WSSeverEvent
 {
     /// <summary>
     /// 服务器连接事件
     /// </summary>
-    public sealed class ConnectionEventArgs : BaseServerEventArgs
+    public sealed class ConnectionEventArgs : System.EventArgs
     {
         #region 属性
         /// <summary>
         /// 客户端类型
         /// </summary>
-        public string Role { get; set; }
+        public string Role { get; private set; }
+
+        /// <summary>
+        /// 机器人登录账号UID
+        /// </summary>
+        public long SelfId { get; private set; }
         #endregion
 
         #region 构造函数
-        internal ConnectionEventArgs(string role, IWebSocketConnectionInfo connectionInfo)
+        internal ConnectionEventArgs(string role, long selfId)
         {
-            
-            this.Role           = role;
-            base.ConnectionInfo = connectionInfo;
+            this.SelfId = selfId;
+            this.Role   = role;
         }
         #endregion
     }
