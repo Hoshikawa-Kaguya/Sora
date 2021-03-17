efea8835c7ce8555fef1d96b7d8c11fb505a7e23
Yukari316 <cbgancna@gmail.com>
2021-03-17 22:55:49 +0800

    去掉奇奇怪怪的东西

--- Sora_Test/Commands.cs
+++ Sora_Test/Commands.cs
@@ -11,11 +11,6 @@ namespace Sora_Test
     [CommandGroup]
     public class Commands
     {
-        /// <summary>
-        /// 请求表
-        /// </summary>
-        private List<User> requestList { get; } = new();
-
         [GroupCommand(CommandExpressions = new[] {"好耶", "哇噢"})]
         public async ValueTask TestCommand1(GroupMessageEventArgs eventArgs)
         {
@@ -26,31 +21,6 @@ namespace Sora_Test
         public async ValueTask TestCommand2(GroupMessageEventArgs eventArgs)
         {
             await eventArgs.Reply("爪巴");
-        }
-
-        [GroupCommand(CommandExpressions = new[] {"pixiv搜图"})]
-        public async ValueTask TestCommand3(GroupMessageEventArgs eventArgs)
-        {
-            if (requestList.Exists(member => member == eventArgs.Sender))
-            {
-                await eventArgs.Reply("dnmd图呢");
-                return;
-            }
-
-            await eventArgs.Reply("图呢");
-            requestList.Add(eventArgs.Sender);
-        }
-
-        [GroupCommand(CommandExpressions = new[] {@"^\[CQ:image,file=[a-z0-9]+\.image\]$"},
-                      MatchType          = MatchType.Regex)]
-        public async ValueTask TestCommand4(GroupMessageEventArgs eventArgs)
-        {
-            if (!requestList.Exists(member => member == eventArgs.Sender)) return;
-            Log.Debug("pic", $"get pic {eventArgs.Message.RawText} searching...");
-            requestList.RemoveAll(user => user == eventArgs.Sender);
-
-            await eventArgs.Reply(await SaucenaoSearch.SearchByUrl("",
-                                                                   eventArgs.Message.GetAllImage()[0].Url, eventArgs));
         }
     }
 }
