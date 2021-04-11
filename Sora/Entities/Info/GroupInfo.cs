23a8c4cf14551851d336cb3df4385130c190bc20
Yukari316 <cbgancna@gmail.com>
2021-04-11 19:40:14 +0800

    GroupInfo支持更多的字段 AT CQ码支持名字覆盖 优化部分API数据处理

--- Sora/Entities/Info/GroupInfo.cs
+++ Sora/Entities/Info/GroupInfo.cs
@@ -1,3 +1,7 @@
+using System;
+using Newtonsoft.Json;
+using YukariToolBox.Time;
+
 namespace Sora.Entities.Info
 {
     /// <summary>
@@ -10,22 +14,55 @@ namespace Sora.Entities.Info
         /// <summary>
         /// 群名称
         /// </summary>
+        [JsonProperty(PropertyName = "group_name")]
         public string GroupName { get; internal init; }
 
         /// <summary>
         /// 成员数
         /// </summary>
+        [JsonProperty(PropertyName = "member_count")]
         public int MemberCount { get; internal init; }
 
         /// <summary>
         /// 最大成员数（群容量）
         /// </summary>
+        [JsonProperty(PropertyName = "max_member_count")]
         public int MaxMemberCount { get; internal init; }
 
         /// <summary>
         /// 群组ID
         /// </summary>
+        [JsonProperty(PropertyName = "group_id")]
         public long GroupId { get; internal init; }
+
+        /// <summary>
+        /// 群备注
+        /// </summary>
+        [JsonProperty(PropertyName = "group_memo", NullValueHandling = NullValueHandling.Ignore)]
+        public string GroupMemo { get; internal init; }
+
+        /// <summary>
+        /// 群创建时间
+        /// </summary>
+        [JsonIgnore]
+        public DateTime? GroupCreateTime { get; internal init; }
+
+        /// <summary>
+        /// 群创建时间
+        /// </summary>
+        [JsonProperty(PropertyName         = "group_create_time", NullValueHandling = NullValueHandling.Ignore,
+                      DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private long? GroupCreateTimeStamp
+        {
+            get => GroupCreateTime?.ToTimeStamp();
+            init => value?.ToDateTime();
+        }
+
+        /// <summary>
+        /// 群等级
+        /// </summary>
+        [JsonProperty(PropertyName = "group_level", NullValueHandling = NullValueHandling.Ignore)]
+        public int? GroupLevel { get; internal init; }
 
         #endregion
     }
