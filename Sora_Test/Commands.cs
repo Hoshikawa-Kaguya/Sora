7515a90fc1a1658d3f6387b8a54dde0116d597a2
Yukari316 <cbgancna@gmail.com>
2021-05-11 18:06:04 +0800

    删除没有用的依赖

--- Sora_Test/Commands.cs
+++ Sora_Test/Commands.cs
@@ -1,14 +1,12 @@
 using Sora.Attributes.Command;
 using Sora.EventArgs.SoraEvent;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 
 namespace Sora_Test
 {
     [CommandGroup]
     public static class Commands
     {
-        [UsedImplicitly]
         [GroupCommand(CommandExpressions = new[] {"1"})]
         public static async ValueTask TestCommand(GroupMessageEventArgs eventArgs)
         {
