30fe8e9fcfbbaf497a7803c2711c270cb44a0c6f
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2021-06-07 16:52:21 +0800

    增加性别枚举

---
+++ Sora/Enumeration/Sex.cs
@@ -0,0 +1,23 @@
+﻿namespace Sora.Enumeration
+{
+    /// <summary>
+    /// 性别
+    /// </summary>
+    public enum Sex
+    {
+        /// <summary>
+        /// 男
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// 女
+        /// </summary>
+        Female,
+
+        /// <summary>
+        /// 未知
+        /// </summary>
+        Unknown
+    }
+}
