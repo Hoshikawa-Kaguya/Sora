c62906c363fbcae5a76af50ab20a967100ec18c1
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2021-11-09 19:32:36 +0800

    ci update .net6

--- Sora_Test/Commands.cs
+++ Sora_Test/Commands.cs
@@ -5,19 +5,18 @@ using Sora.Entities.Segment;
 using Sora.Entities.Segment.DataModel;
 using YukariToolBox.FormatLog;
 
-namespace Sora_Test
+namespace Sora_Test;
+
+[CommandGroup]
+public static class Commands
 {
-    [CommandGroup]
-    public static class Commands
+    [GroupCommand(CommandExpressions = new[] {"1"}, Description = "死了啦都你害的啦")]
+    public static async ValueTask TestCommand(GroupMessageEventArgs eventArgs)
     {
-        [GroupCommand(CommandExpressions = new[] { "1" }, Description = "死了啦都你害的啦")]
-        public static async ValueTask TestCommand(GroupMessageEventArgs eventArgs)
-        {
-            var s = eventArgs.Message.MessageBody[0].Data as TextSegment;
-            Log.Info("触发指令", $"txet:{s!.Content}");
-            eventArgs.IsContinueEventChain = false;
-            //throw new Exception("shit");
-            await eventArgs.Reply(SoraSegment.At(eventArgs.Sender) + "怪欸");
-        }
+        var s = eventArgs.Message.MessageBody[0].Data as TextSegment;
+        Log.Info("触发指令", $"txet:{s!.Content}");
+        eventArgs.IsContinueEventChain = false;
+        //throw new Exception("shit");
+        await eventArgs.Reply(SoraSegment.At(eventArgs.Sender) + "怪欸");
     }
 }
