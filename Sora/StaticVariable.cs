b44f77f80c350d9bea17fed9d8a0f362e5b0a5df
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2021-11-16 02:02:52 +0800

    更新依赖

--- Sora/StaticVariable.cs
+++ Sora/StaticVariable.cs
@@ -43,7 +43,7 @@ public static class StaticVariable
     /// <summary>
     /// 版本号
     /// </summary>
-    public const string Version = "1.0.0-rc26";
+    public const string Version = "1.0.0-rc29";
 
     /// <summary>
     /// 清除服务数据
