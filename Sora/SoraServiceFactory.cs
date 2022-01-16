1ceeb71344ac4c4b03d0238fef5cabbd30a15cff
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2022-01-17 03:33:10 +0800

    修改命名

--- Sora/SoraServiceFactory.cs
+++ Sora/SoraServiceFactory.cs
@@ -20,7 +20,7 @@ public static class SoraServiceFactory
     /// </summary>
     static SoraServiceFactory()
     {
-        Log.Info("Sora", $"框架版本:{StaticVariable.Version}");
+        Log.Info("Sora", $"框架版本:{StaticVariable.VERSION}");
         Log.Debug("Sora", "开发交流群：1081190562");
         Log.Debug("System", Environment.OSVersion.ToString());
         Log.Debug("Runtime", Environment.Version.ToString());
