c89fb8ad15367d77432329f5a9dbec528c213f97
Yukari316 <cbgancna@gmail.com>
2022-02-01 22:57:32 +0800

    修复错误的指令判断 #52

--- Sora_Test/Commands.cs
+++ Sora_Test/Commands.cs
@@ -1,9 +1,8 @@
-using System.Threading.Tasks;
 using Sora.Attributes.Command;
 using Sora.Entities.Segment.DataModel;
 using Sora.Enumeration;
-using Sora.Enumeration.EventParamsType;
 using Sora.EventArgs.SoraEvent;
+using System.Threading.Tasks;
 using YukariToolBox.LightLog;
 
 namespace Sora_Test;
@@ -14,13 +13,11 @@ public static class Commands
     [SoraCommand(
         CommandExpressions = new[] {"1"},
         Description = "死了啦都你害的啦",
-        SourceType = SourceFlag.Group,
-        PermissionLevel = MemberRoleType.Admin,
-        Priority = 10)]
+        SourceType = SourceFlag.Group)]
     public static async ValueTask TestCommand1(GroupMessageEventArgs eventArgs)
     {
         eventArgs.IsContinueEventChain = false;
-
+    
         var s = eventArgs.Message.MessageBody[0].Data as TextSegment;
         Log.Info("触发指令", $"text:{s!.Content}");
         await eventArgs.Reply($"哇哦");
