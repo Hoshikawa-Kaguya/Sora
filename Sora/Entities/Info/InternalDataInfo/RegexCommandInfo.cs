7996b3dbb8fbd69ad223813416478e3992277c45
Yukari316 <cbgancna@gmail.com>
2022-04-14 17:52:49 +0800

    feat: 指令组名，指令组的启用和禁用

--- Sora/Entities/Info/InternalDataInfo/RegexCommandInfo.cs
+++ Sora/Entities/Info/InternalDataInfo/RegexCommandInfo.cs
@@ -11,6 +11,7 @@ namespace Sora.Entities.Info.InternalDataInfo;
 /// </summary>
 internal sealed record RegexCommandInfo : BaseCommandInfo
 {
+    internal readonly string     ClassName;    //指令类名
     internal readonly string     GroupName;    //指令组名
     internal readonly MethodInfo MethodInfo;   //指令执行的方法
     internal readonly Type       InstanceType; //指令所在实例类型
@@ -19,13 +20,14 @@ internal sealed record RegexCommandInfo : BaseCommandInfo
     /// 指令信息构造(常规指令构建)
     /// </summary>
     internal RegexCommandInfo(
-        string            desc,             string[] regex,        string       groupName,    MethodInfo method,
-        MemberRoleType    permissionType,   int      priority,     RegexOptions regexOptions, SourceFlag source,
-        Action<Exception> exceptionHandler, long[]   sourceGroups, long[]       sourceUsers,  bool       suCommand,
-        Type              instanceType = null)
+        string     desc,      string[]          regex,            string className,    string       groupName,
+        MethodInfo method,    MemberRoleType    permissionType,   int    priority,     RegexOptions regexOptions,
+        SourceFlag source,    Action<Exception> exceptionHandler, long[] sourceGroups, long[]       sourceUsers,
+        bool       suCommand, Type              instanceType = null)
         : base(desc, regex, permissionType, suCommand, priority, regexOptions, source, exceptionHandler, sourceGroups,
             sourceUsers)
     {
+        ClassName    = className;
         GroupName    = groupName;
         MethodInfo   = method;
         InstanceType = instanceType;
