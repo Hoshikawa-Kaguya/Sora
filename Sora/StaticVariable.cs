22a760f4db848a2c8ac4d034c976f5756dce3116
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2022-04-27 16:40:47 +0800

    release: 新版本号1.0.0-rc51

--- Sora/StaticVariable.cs
+++ Sora/StaticVariable.cs
@@ -44,7 +44,7 @@ public static class StaticVariable
     /// <summary>
     /// 版本号
     /// </summary>
-    public const string VERSION = "1.0.0-rc50";
+    public const string VERSION = "1.0.0-rc51";
 
     /// <summary>
     /// Onebot版本
