19a2ed2791646e9f771e0a64bf25179182a49ce9
Yukari316 <cbgancna@gmail.com>
2022-06-14 23:57:52 +0800

    release: 1.0.0-rc60

--- Sora/StaticVariable.cs
+++ Sora/StaticVariable.cs
@@ -44,7 +44,7 @@ public static class StaticVariable
     /// <summary>
     /// 版本号
     /// </summary>
-    public const string VERSION = "1.0.0-rc59";
+    public const string VERSION = "1.0.0-rc60";
 
     /// <summary>
     /// Onebot版本
