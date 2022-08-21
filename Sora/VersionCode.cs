325476c2f0f40d5f29b7147763fdefd60f7dcb87
Yukari316 <7535224+Yukari316@users.noreply.github.com>
2022-08-22 04:43:15 +0800

    release: 1.0.0-rc76

--- Sora/VersionCode.cs
+++ Sora/VersionCode.cs
@@ -8,7 +8,7 @@ public static class VersionCode
     /// <summary>
     /// 版本号
     /// </summary>
-    public const string VERSION = "1.0.0-rc75";
+    public const string VERSION = "1.0.0-rc76";
 
     /// <summary>
     /// Onebot版本
