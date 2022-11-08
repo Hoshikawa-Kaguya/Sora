7a2519a00a08ce367e6d9bde63bd43fad3c6123b
Yukari316 <cbgancna@gmail.com>
2022-11-08 18:44:53 +0800

    release: 1.0.0-rc79

--- Sora/VersionCode.cs
+++ Sora/VersionCode.cs
@@ -8,7 +8,7 @@ public static class VersionCode
     /// <summary>
     /// 版本号
     /// </summary>
-    public const string VERSION = "1.0.0-rc78";
+    public const string VERSION = "1.0.0-rc79";
 
     /// <summary>
     /// Onebot版本
