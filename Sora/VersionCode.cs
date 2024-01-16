f5218cf784a461102b2c141c878af0541e7d61d1
脆饼干 <cbgancna@gmail.com>
2024-01-16 17:37:58 +0800

    release: 1.4.3

--- Sora/VersionCode.cs
+++ Sora/VersionCode.cs
@@ -8,7 +8,7 @@ public static class VersionCode
     /// <summary>
     /// 版本号
     /// </summary>
-    public const string VERSION = "1.4.2";
+    public const string VERSION = "1.4.3";
 
     /// <summary>
     /// Onebot版本
